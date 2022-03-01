572597dbf0a14a19d6b1d413154f3d2a18dcfbe9
Chupriarti <wallx13@gmail.com>
2022-03-01 23:02:50 +0300

    add tests with describe

--- src/validateValue/validateValue.test.js
+++ src/validateValue/validateValue.test.js
@@ -3,3 +3,15 @@ const validateValue = require('./validateValue')
 test('Validate value', () => {
     expect(validateValue(50)).toBe(true)
 })
+
+describe('validateValue', () => {
+    test('Correct value', () => {
+        expect(validateValue(50)).toBe(true)
+    })
+    test('Large value', () => {
+        expect(validateValue(-1)).toBe(false)
+    })
+    test('Less value', () => {
+        expect(validateValue(101)).toBe(false)
+    })
+})
