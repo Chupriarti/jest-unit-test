9845d26bd042bd8104c81afed921120d5aff6170
Chupriarti <wallx13@gmail.com>
2022-03-02 23:08:30 +0300

    add other tests for array

--- src/mapArrToStrings/mapArrToStrings.test.js
+++ src/mapArrToStrings/mapArrToStrings.test.js
@@ -4,4 +4,13 @@ describe('mapArrToStrings', () => {
     test('Correct value', () => {
         expect(validateValue([1, 2, 3])).toEqual(['1', '2', '3'])
     })
+    test('Bad values', () => {
+        expect(validateValue([1, 2, 3, null, undefined, 'assfasd'])).toEqual(['1', '2', '3'])
+    })
+    test('Empty array', () => {
+        expect(validateValue([])).toEqual([])
+    })
+    test('Not equal value', () => {
+        expect(validateValue([1, 2, 3])).not.toEqual([1, 2, 3, 4])
+    })
 })
