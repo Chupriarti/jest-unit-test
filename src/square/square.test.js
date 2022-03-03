97e8a1c266221f3c8a256ed92ef21a6abf48d054
Chupriarti <wallx13@gmail.com>
2022-03-03 22:25:02 +0300

    prepare jest cycle hooks

--- src/square/square.test.js
+++ src/square/square.test.js
@@ -1,10 +1,27 @@
 const square = require('./square')
 
 describe('square', () => {
+
+    beforeEach(() => {
+
+    })
+
+    beforeAll(() => {
+            
+    })
+
     test('Correct value', () => {
         expect(square(2)).toBe(4);
         expect(square(2)).toBeLessThan(5);
         expect(square(2)).toBeGreaterThan(3);
         expect(square(2)).not.toBeUndefined();
+    })
+
+    afterEach(() => {
+            
+    })
+
+    afterAll(() => {
+
     })
 })
