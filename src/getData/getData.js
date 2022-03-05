58508bb1050a49d4a5ccd0338f858f2427345825
Chupriarti <wallx13@gmail.com>
2022-03-05 23:19:37 +0300

    reuse previous function

--- src/getData/getData.js
+++ src/getData/getData.js
@@ -1,7 +1,8 @@
 const axios = require('axios')
+const mapArrToStrings = require('../mapArrToStrings/mapArrToStrings')
 
 const getData = () => {
     const response = await axios.get('https://jsonplaceholder.typicode.com/users')
     const userIds = response.data.map(user => user.id)
-    return userIds
+    return mapArrToStrings(userIds)
 }
